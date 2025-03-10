8bf54586c856fff6285eb2dc068fc37023eb2bf4
pononokp <pononokp@unb.ca>
2025-03-10 10:51:00 -0300

    Update Timeline.tsx

--- src/components/skill/Timeline.tsx
+++ src/components/skill/Timeline.tsx
@@ -112,7 +112,7 @@ const Timeline = () => {
                     icon={javaIcon}
                     intersectionObserverProps={{
                         onChange: handleIntersectionChange,
-                        threshold: 0.1, // Element must be at least 10% visible to trigger
+                        threshold: 0.3, // Element must be at least 10% visible to trigger
                     }}
                 >
                     <h3 className="vertical-timeline-element-title">
@@ -162,7 +162,7 @@ const Timeline = () => {
                     icon={pythonIcon}
                     intersectionObserverProps={{
                         onChange: handleIntersectionChange,
-                        threshold: 0.1, // Element must be at least 10% visible to trigger
+                        threshold: 0.3, // Element must be at least 10% visible to trigger
                     }}
                 >
                     <h3 className="vertical-timeline-element-title">
